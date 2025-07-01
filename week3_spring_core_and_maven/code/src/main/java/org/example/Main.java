9899bf0a2e7e77c21cca88c3afd0e306ea7b25ad
Stuti Sinha <stutisinha712@gmail.com>
2025-07-01 21:11:11 +0530

    Refactor code structure for improved readability and maintainability

---
+++ week3_spring_core_and_maven/code/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Use the service
+        bookService.addBook("The Great Gatsby");
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
