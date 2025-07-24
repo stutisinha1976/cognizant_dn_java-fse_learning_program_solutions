dfc51f1b846f4a8f21853f46269d8b48b239a823
Stuti Sinha <stutisinha712@gmail.com>
2025-07-24 14:09:50 +0530

    week 6 react assn 1

---
+++ week6_React/1_create_react_app/code/my-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the first session of React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
