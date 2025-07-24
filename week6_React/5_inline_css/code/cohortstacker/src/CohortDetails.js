3d4f65e6d826c7e1407526718e7ac7892fe4fe7e
Stuti Sinha <stutisinha712@gmail.com>
2025-07-24 23:06:09 +0530

    assn 5 inline css

---
+++ week6_React/5_inline_css/code/cohortstacker/src/CohortDetails.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './CohortDetails.module.css';
+
+function CohortDetails(props) {
+  const { cohort } = props;
+
+  // Step 10: dynamic inline color for h3
+  const headingStyle = {
+    color: cohort.currentStatus === 'Ongoing' ? 'green' : 'blue'
+  };
+
+  return (
+    <div className={styles.box}>
+      <h3 style={headingStyle}>
+        {cohort.cohortCode} -
+        <span> {cohort.technology}</span>
+      </h3>
+      <dl>
+        <dt>Started On</dt>
+        <dd>{cohort.startDate}</dd>
+        <dt>Current Status</dt>
+        <dd>{cohort.currentStatus}</dd>
+        <dt>Coach</dt>
+        <dd>{cohort.coachName}</dd>
+        <dt>Trainer</dt>
+        <dd>{cohort.trainerName}</dd>
+      </dl>
+    </div>
+  );
+}
+
+export default CohortDetails;
